70ea08b6ebfc8d3b46722bafeccea63d025bbf58
Sarthak Agarwal <69386523+Sarthak-Agarwal1410@users.noreply.github.com>
2022-12-22 18:53:58 +0530

    Update mytest.sh

--- mytest.sh
+++ mytest.sh
@@ -1,7 +1,7 @@
-echo "Hey everyone this is my Jenkins dashboard"
-date
-cal
-echo "bye"
-echo "thank you"
-echo "code has edited now"
-echo "new code after ngrok setup"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+sudo chown root:root /usr/bin/chromedriver
+sudo chmod +x /usr/bin/chromedriver
