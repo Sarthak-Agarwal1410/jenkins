1e9554d97f211683ec1fbd3374c7dd7cf3983b9f
Sarthak Agarwal <69386523+Sarthak-Agarwal1410@users.noreply.github.com>
2022-12-23 18:28:55 +0530

    Update and rename mytest.sh to Jenkinsfile

--- mytest.sh
+++
@@ -1,8 +0,0 @@
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
-wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
-sudo apt-get -y install zip
-unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/bin/chromedriver
-sudo chown root:root /usr/bin/chromedriver
-sudo chmod +x /usr/bin/chromedriver
